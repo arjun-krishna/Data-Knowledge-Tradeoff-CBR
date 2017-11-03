2ead59afbf7424aa190b8b1ca1534e77cee9839b
arjun-krishna <arjunkrishna.iitm@gmail.com>
2017-11-03 23:26:15 +0530

    XML parsing for fuzzy attribute

--- src/FCBR/Parser.java
+++ src/FCBR/Parser.java
@@ -3,8 +3,49 @@ package FCBR;
 import java.io.*;
 import java.util.*;
 
+import org.jdom2.Comment;
+import org.jdom2.Content;
+import org.jdom2.Content.CType;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.filter.Filters;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.util.IteratorIterable;
+
 public class Parser {
 	public void info() {
 		System.err.println("Parser\n-> Read XML documents of the fuzzy attributes");
 	}	
+	public void parse(String file_name) throws JDOMException, IOException {
+
+		File input_file = new File(file_name); 
+		SAXBuilder saxBuilder = new SAXBuilder(); 
+		Document document = saxBuilder.build(input_file);
+
+		System.err.println("Parsing : "+file_name);
+
+		Element root = document.getRootElement();
+		System.err.println("Identified Root -> "+root.getName());
+		
+		List<Element> attributes = root.getChildren();
+
+		for (int i=0; i<attributes.size(); i++) {
+			Element attribute = attributes.get(i);
+			System.out.println(attribute.getName());
+			System.out.println('\t'+attribute.getChildText("name")+" ( "+ attribute.getChildText("type")+" ) ");
+			
+			Element fuzzy_categories = attribute.getChild("levels");
+
+			List<Element> levels = fuzzy_categories.getChildren();
+
+			for (int j=0; j<levels.size(); j++) {
+				Element value = levels.get(j);
+				System.out.println("\t\t"+value.getText());
+
+			}
+
+		}
+
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 import java.io.*;
 import java.util.*;
 
+import org.jdom2.JDOMException;
+
 import FCBR.Parser;
 
 public class Main {
 	public static void main(String args[]) {
 		Parser P = new Parser();
 		P.info();
+		try {
+			P.parse("TermPaperExample/attributes.xml");
+		} catch(JDOMException err) {
+			System.err.println(err);
+		} catch (IOException err) {
+			System.err.println(err);
+		}
 	}
 }
