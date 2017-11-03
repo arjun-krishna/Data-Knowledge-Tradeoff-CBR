8f79e473d16411b507b675125ab7a88af04cc71c
arjun-krishna <arjunkrishna.iitm@gmail.com>
2017-11-03 22:39:32 +0530

    Adding package structure for Fuzzy CBR

---
+++ src/FCBR/Parser.java
@@ -0,0 +1,10 @@
+package FCBR;
+
+import java.io.*;
+import java.util.*;
+
+public class Parser {
+	public void info() {
+		System.err.println("Parser\n-> Read XML documents of the fuzzy attributes");
+	}	
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+import FCBR.Parser;
+
+public class Main {
+	public static void main(String args[]) {
+		Parser P = new Parser();
+		P.info();
+	}
+}
