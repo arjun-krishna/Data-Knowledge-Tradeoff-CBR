63fd3c17232c5da4b3b44a33dbccfbc05c679df2
arjun-krishna <arjunkrishna.iitm@gmail.com>
2017-11-23 14:16:03 +0530

    jFuzzy other Defuzzifiers

--- jFuzzyLogic/TermPaper.java
+++ jFuzzyLogic/TermPaper.java
@@ -3,6 +3,9 @@ import net.sourceforge.jFuzzyLogic.FunctionBlock;
 import net.sourceforge.jFuzzyLogic.Gpr;
 import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
 import net.sourceforge.jFuzzyLogic.rule.Variable;
+import net.sourceforge.jFuzzyLogic.defuzzifier.Defuzzifier;
+import net.sourceforge.jFuzzyLogic.defuzzifier.DefuzzifierCenterOfArea;
+
 
 public class TermPaper {
     public static void main(String[] args) throws Exception {
@@ -20,14 +23,21 @@ public class TermPaper {
         // JFuzzyChart.get().chart(functionBlock);
 
         // Set inputs
-        functionBlock.setVariable("days_late", 8);
-        functionBlock.setVariable("medical_certificate", 0);
+        functionBlock.setVariable("days_late", 3);
+        functionBlock.setVariable("medical_certificate", 1);
 
         // Evaluate
         functionBlock.evaluate();
 
         // Get output
         Variable acceptance = functionBlock.getVariable("acceptance");
+        
+        Defuzzifier COA = new DefuzzifierCenterOfArea(acceptance);
+
+
+        System.out.println("A[COA] = "+COA.defuzzify());
+
+
         JFuzzyChart.get().chart(acceptance, acceptance.getDefuzzifier(), true);
 
         Gpr.debug("low[days_late]: "+ functionBlock.getVariable("days_late").getMembership("low"));
@@ -35,5 +45,9 @@ public class TermPaper {
 
         // System.out.println(functionBlock);
         System.out.println("Acceptance : "+ functionBlock.getVariable("acceptance").getValue());
+        System.out.println("A[MAX] = "+acceptance.getUniverseMax());
+        System.out.println("A[MIN] = "+acceptance.getUniverseMin());
     }
 }
+
+// http://jfuzzylogic.sourceforge.net/html/manual.html#runfcl
