b064c8394c083ea4b95ecf813f4348a9c7f21544
arjun-krishna <arjunkrishna.iitm@gmail.com>
2017-11-23 16:32:00 +0530

    Term Paper finalized!

--- src/TermPaper/TermPaperCBR.java
+++ src/TermPaper/TermPaperCBR.java
@@ -21,13 +21,24 @@ import net.sourceforge.jFuzzyLogic.Gpr;
 import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
 import net.sourceforge.jFuzzyLogic.rule.Variable;
 
-public class TermPaperCBR{
+
+public class TermPaperCBR {
 
 	public static ArrayList<TermPaper> cases;
 	public static HashMap<String,TermPaper> case_map;
+	public static FIS fis;
+
+	// Params
+	public static boolean DISPLAY_DEFUZZIFICATION = false;
+	public static boolean INTERACTIVE_INTERFACE = true;
+	public static boolean CASE_DELETION = false;
+	public static boolean ACCURACY_CHECK = true;
 
 	public static void main (String[] args) throws Exception{
+
+		// read dataset
 		getCases(args[0]);
+	
 		try {
 			Project p = new Project();
 			
@@ -35,127 +46,210 @@ public class TermPaperCBR{
 			Concept termpaper = p.createTopConcept("TermPaper");
 			
 			// Add descriptions for the concepts along with their domains. Look for classes of the type *Desc.
-			IntegerDesc daysDesc = new IntegerDesc(termpaper,"days_late",0,20);
-			BooleanDesc medcertDesc = new BooleanDesc(termpaper,"med_cert");
+			IntegerDesc daysDesc = new IntegerDesc(termpaper, "days_late", 0, 20);
+			BooleanDesc medcertDesc = new BooleanDesc(termpaper, "med_cert");
 			
 			// Add a similarity function for the attribute using the add*Fct function. The function types can be specified(refer documentation)
-			IntegerFct dayFct = daysDesc.addIntegerFct("dayfct",true);
+			IntegerFct dayFct = daysDesc.addIntegerFct("dayfct", true);
 			dayFct.setFunctionTypeL(NumberConfig.POLYNOMIAL_WITH);
 			dayFct.setFunctionTypeR(NumberConfig.POLYNOMIAL_WITH);
 			
 			// For the boolean variable, the similarity is 0-1.
 			
-			
 			// Create an amalgamation function, to define the global similarity. // Add the amalgamation function for the concept, so that this global similarity is used.
-			AmalgamationFct glob_sim = termpaper.addAmalgamationFct(AmalgamationConfig.WEIGHTED_SUM,"TermPaper",true);
-			glob_sim.setActiveFct(daysDesc,dayFct);
+			AmalgamationFct glob_sim = termpaper.addAmalgamationFct(AmalgamationConfig.WEIGHTED_SUM, "TermPaper", true);
+			glob_sim.setActiveFct(daysDesc, dayFct);
+
 			//Change the weights here.
 			glob_sim.setWeight(daysDesc,3);
 			glob_sim.setWeight(medcertDesc,2);
 
-			// add casebase
-			
+			// create casebase
 			DefaultCaseBase cb = p.createDefaultCB("myCaseBase");
 			
 			// add instances to the casebase
 			Instance i;
-			for(int j=0;j<cases.size();j++){
+			for (int j=0; j<cases.size(); j++) {
 				i = termpaper.addInstance("tp"+j);
-				i.addAttribute(daysDesc,cases.get(j).days_late);
-				i.addAttribute(medcertDesc,cases.get(j).med_cert);
+				i.addAttribute(daysDesc, cases.get(j).days_late);
+				i.addAttribute(medcertDesc, cases.get(j).med_cert);
 				cb.addCase(i);
-				case_map.put("tp"+j,cases.get(j));				
+				case_map.put("tp"+j, cases.get(j));				
 			}
 			
 			// set up query and retrieval. the retrieval method, number of cases to retrieve can be set
 			
-			Retrieval r = new Retrieval(termpaper,cb);
-			r.setRetrievalMethod(Retrieval.RetrievalMethod.RETRIEVE_K_SORTED);
-			r.setK(3);
-			Instance q = r.getQueryInstance();
-			
-			// Get input
-			Scanner in = new Scanner(System.in);
-			int days;
-			boolean med_cert;
-			System.out.print("Enter the number of days late: ");
-			days = in.nextInt();
-			System.out.print("Is medical certificate submitted? Enter \"true\" or \"false\": ");
-			med_cert = in.nextBoolean();	
-			q.addAttribute(daysDesc.getName(),days);
-			q.addAttribute(medcertDesc.getName(),med_cert);
-			
-			int med_int;
-			med_int = med_cert? 1:0;
-			double acceptance = fuzzyReason(args[1],days,med_int);
-			System.out.println();
-			if(acceptance >= 0.4 && acceptance <= 0.6){
-				r.start();
-				boolean isAccepted = getPrediction(r);
-				if(isAccepted){
-					System.out.println("Term paper Accepted(using CB)");
+			Retrieval retriever = new Retrieval(termpaper, cb);
+			retriever.setRetrievalMethod(Retrieval.RetrievalMethod.RETRIEVE_K_SORTED);
+			retriever.setK(3);
+
+			// Fuzzy Reasoner Initialization
+			fis = FIS.load(args[1], true);
+			if( fis == null ) { 
+        System.err.println("Can't load file => " + args[1]);
+        System.exit(0);
+	    }
+
+	    // Case Accuracy
+	    if (ACCURACY_CHECK) {
+	    	double accuracy = 0.0;
+	    	for (int j=0; j<cases.size(); j++) {
+					TermPaper tp = cases.get(j);
+					int days_late = tp.days_late;
+					int med_cert = tp.med_cert ? 1 : 0;
+
+					boolean isAccepted = false;
+
+					double acceptance = fuzzyReason(days_late, med_cert, false);
+
+					if (acceptance >= 0.4 && acceptance <= 0.6) {
+						Instance q = retriever.getQueryInstance();
+						q.addAttribute(daysDesc.getName(), days_late);
+						q.addAttribute(medcertDesc.getName(), med_cert);
+
+						retriever.start();
+						isAccepted = getPrediction(retriever);
+
+					} else if (acceptance < 0.4) {
+						isAccepted = false;
+					} else {
+						isAccepted = true;
+					}
+					if (isAccepted == tp.accepted) {
+						accuracy += 1;
+					}
 				}
-				else{
-					System.out.println("Term paper Rejected(using CB)");
+				accuracy /= cases.size();
+				System.out.println("Accuracy on Case Base = "+String.format("%.02f", accuracy*100)+"%");
+	    }
+
+			// Case Base Reduction
+			if (CASE_DELETION) {
+				for (int j=0; j<cases.size(); j++) {
+					TermPaper tp = cases.get(j);
+					int days_late = tp.days_late;
+					int med_cert = tp.med_cert ? 1 : 0;
+
+					double acceptance = fuzzyReason(days_late, med_cert, false);
+
+					if (acceptance >= 0.4 && acceptance <= 0.6) {
+						System.err.println("Case Needed ("+days_late+" , "+med_cert+" , "+acceptance+" )");
+					}
 				}
 			}
-			else if(acceptance < 0.4){
-				System.out.println("Term paper Rejected(without using CB)");
+
+			
+			if (INTERACTIVE_INTERFACE) {
+				// Get input
+				Scanner in = new Scanner(System.in);
+
+				int days;
+				boolean med_cert;
+				
+				System.out.print("Enter the number of days late: ");
+				days = in.nextInt();
+				
+				System.out.print("Is medical certificate submitted? Enter \"true\" or \"false\": ");
+				med_cert = in.nextBoolean();	
+				
+				Instance q = retriever.getQueryInstance();
+				q.addAttribute(daysDesc.getName(), days);
+				q.addAttribute(medcertDesc.getName(), med_cert);
+				
+				int med_int;
+				med_int = med_cert? 1 : 0;
+
+				double acceptance = fuzzyReason(days, med_int, DISPLAY_DEFUZZIFICATION);
+				System.out.println();
+
+				if(acceptance >= 0.4 && acceptance <= 0.6) {
+					retriever.start();
+					boolean isAccepted = getPrediction(retriever);
+					if(isAccepted) {
+						System.out.println("Term paper Accepted (using CB)");
+					}
+					else {
+						System.out.println("Term paper Rejected (using CB)");
+					}
+				}
+				else if(acceptance < 0.4) {
+					System.out.println("Term paper Rejected (without using CB)");
+				}
+				else {
+					System.out.println("Term paper Accepted (without using CB)");
+				}
 			}
-			else{
-				System.out.println("Term paper Accepted(without using CB)");
-			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
 	}
 	
-	public static double fuzzyReason(String filename, int days, int med_cert){
-		// Setup the fuzzy reasoner
-		    FIS fis = FIS.load(filename,true);
-		    
-		    if( fis == null ) { 
-		        System.err.println("Can't load file => " + filename);
-		        return 0.0;
-		    }
+	public static double fuzzyReason(int days, int med_cert, boolean display) {
 
-        	FunctionBlock functionBlock = fis.getFunctionBlock(null);
-        	functionBlock.setVariable("days_late", days);
-			functionBlock.setVariable("medical_certificate", med_cert);
+  	FunctionBlock functionBlock = fis.getFunctionBlock(null);
+  	
+  	functionBlock.setVariable("days_late", days);
+		functionBlock.setVariable("medical_certificate", med_cert);
 
-			// Evaluate
-			functionBlock.evaluate();
+		// Evaluate
+		functionBlock.evaluate();
 
-			// Get output
-			Variable acceptance = functionBlock.getVariable("acceptance");
-			JFuzzyChart.get().chart(acceptance, acceptance.getDefuzzifier(), false);
-			
-			return functionBlock.getVariable("acceptance").getValue();
+		// Get output
+		Variable acceptance = functionBlock.getVariable("acceptance");
+		
+		// Plot Defuzzification
+		JFuzzyChart.get().chart(acceptance, acceptance.getDefuzzifier(), display);
+		
+		return functionBlock.getVariable("acceptance").getValue();
 	}
 
 	private static void print(Retrieval r) {
 		for (Map.Entry<Instance, Similarity> entry: r.entrySet()) {
-			System.out.println("\nSimilarity: " + entry.getValue().getValue()
+			System.err.println("\nSimilarity: " + entry.getValue().getValue()
 					+ " to case: " + entry.getKey().getName());
 		}
 	}
 	
-	public static boolean getPrediction(Retrieval r){
+	public static boolean getPrediction(Retrieval r) {
+		double acceptance_prob = similarityWeighted(r);
+		if (acceptance_prob >= 0.5) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static double similarityWeighted(Retrieval r) {
+		double total = 0.0;
+		int num_cases = 0;
+		for (Map.Entry<Instance, Similarity> entry: r.entrySet()) {
+			if (case_map.get(entry.getKey().getName()).accepted ) {
+				total += entry.getValue().getValue();
+			}
+			num_cases++;
+		}
+		double acceptance_prob = total / num_cases;
+		return acceptance_prob;
+	}
+
+
+	public static double majorityVote(Retrieval r) {
 		int num_true = 0;
 		int num_false = 0;
 		for (Map.Entry<Instance, Similarity> entry: r.entrySet()) {
-			if(case_map.get(entry.getKey().getName()).accepted){
+			if(case_map.get(entry.getKey().getName()).accepted) {
 				num_true += 1;
 			}
-			else{
+			else {
 				num_false += 1;
 			}
 		}
-		if(num_true > num_false){
-			return true;
+		if(num_true > num_false) {
+			return 1.0;
 		}
-		return false;
+		return 0.0;
 	}
 	
 	public static void getCases(String fileName) throws Exception{
@@ -165,19 +259,19 @@ public class TermPaperCBR{
 		String[] line_split;
 		int days;
 		boolean med, acc;
-		for(int i=1;i<lines.size();i++){
+		for(int i=1;i<lines.size();i++) {
 			line_split = lines.get(i).split(",");
 			days = Integer.parseInt(line_split[0]);
-			if(line_split[1].equals("False")){
+			if(line_split[1].equals("False")) {
 				med = false;
 			}
-			else{
+			else {
 				med = true;
 			}
-			if(line_split[2].equals("False")){
+			if(line_split[2].equals("False")) {
 				acc = false;
 			}
-			else{
+			else {
 				acc = true;
 			}
 			cases.add(new TermPaper(days,med,acc));
